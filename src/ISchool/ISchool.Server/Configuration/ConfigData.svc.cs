aba9bdbffeb89ed0e028f6f7a38ac3a7f0574575
El-tayeb Karrar <el.tayeb.karrara1@gmail.com>
2017-01-26 20:18:18 +0300

    Start Server Project

---
+++ src/ISchool/ISchool.Server/Configuration/ConfigData.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ISchool.Server.Configuration
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ConfigData" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select ConfigData.svc or ConfigData.svc.cs at the Solution Explorer and start debugging.
+    public class ConfigData : IConfigData
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
---
+++ src/ISchool/ISchool.Server/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ISchool.Server")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ISchool.Server")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("60bcc18a-d88e-459f-88e1-f80bf4f3cdf5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
