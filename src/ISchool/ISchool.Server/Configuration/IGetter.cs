1065231348db1a01cb10da78ea2a278b2feca255
El-tayeb Karrar <el.tayeb.karrara1@gmail.com>
2017-01-30 02:05:24 +0300

    Night Work Good Night :)

---
+++ src/ISchool/ISchool.Server/Configuration/IGetter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ISchool.Server.Configuration
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IGetter" in both code and config file together.
+    [ServiceContract]
+    public interface IGetter
+    {
+        [OperationContract]
+        int UpdateStudentPhoneNumber(int StudentId, string PhoneNumber);
+        [OperationContract]
+        int UpdateStudentInfo(int StudentId, string Name, string Specialization, string Nationality, string NationalId, string EnterDate, int AcademicSupervisor);
+    }
+}
