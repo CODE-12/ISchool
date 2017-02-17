73eff75449b033dea187a9bbe265556ec265ff76
El-tayeb Karrar <el.tayeb.karrara1@gmail.com>
2017-02-17 12:18:17 +0300

    Append Excel Analizer

---
+++ src/ISchool/ISchool.ExcelAlgorithm/IExcelAlgo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace ISchool.ExcelAlgorithm
+{
+    public abstract class IExcelAlgo
+    {
+        protected string _path { get; set; }
+        public IExcelAlgo(string path)
+        {
+            this._path = path;
+            Task parralelWork = new Task(() => { this.Load(); });
+            parralelWork.Start();
+        }
+
+        protected abstract void Load();
+
+        protected abstract DataTable GetDataShow();
+    }
+}
---
+++ src/ISchool/ISchool.ExcelAlgorithm/StudentDataList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISchool.ExcelAlgorithm
+{
+    public class StudentDataList : IExcelAlgo
+    {
+        /*Start Vars*/
+        private int _start { get; set; }
+        private int _nameC { get; set; }
+        private int _nationalC { get; set; }
+        private int _specialC { get; set; }
+        private int _enterDC { get; set; }
+        private int _idTypeC { get; set; }
+        private int _idNumC { get; set; }
+        private int _brDC { get; set; }
+        private int _statusC { get; set; }
+        private int _gadeTypeC { get; set; }
+        private DataTable _reault { get; set; }
+        /*End Vars*/
+        public StudentDataList(string path) : base(path)
+        {
+
+        }
+
+        protected override void Load()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override DataTable GetDataShow()
+        {
+            return _reault;
+        }
+    }
+}
